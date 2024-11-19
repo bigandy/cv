79f3fd570297f0274b28c580414af736bac99a4f
Andrew Hudson <ahudson@gmail.com>
2024-11-19 15:26:51 +0100

    break down roles into an object, so can loop easier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,102 @@
+<script lang="ts">
+	const roles = {
+		scalable: {
+			title: 'Senior Front-end Engineer',
+			company: 'Scalable',
+			dates: {
+				start: '2023-10-01',
+				end: '2024-08-18'
+			},
+			url: 'https://www.scalable.com',
+			location: 'Remote, UK',
+			summaries: [
+				'Web app development with React.js, Typescript, and D3',
+				'Code review of peers and mentoring of junior developers',
+				'Estimating, planned and documented new features with product and design.',
+				'Creating new features and speccing out API changes with the backend team',
+				'Creating API contract with the backend API developer',
+				'Mocking data with express to simulate the API response until backend finished API work'
+			]
+		},
+		ventureHarbour: {
+			title: 'Senior Front-end Engineer',
+			company: 'Venture Harbour',
+			dates: {
+				start: '2017-10-01',
+				end: '2023-02-28'
+			},
+			url: 'https://www.ventureharour.com',
+			location: 'Remote, UK',
+			summaries: [
+				'Created a component library using Storybook to showcase and document.',
+				'Break down problems into solutions and solutions into tasks, creating prototypes when required.',
+				'Desktop Application and Browser Extension development.',
+				'Optimise Performance of each of our sites and products and automate testing and CI for each deployment.'
+			]
+		},
+		taylorAndFrancis: {
+			title: 'Full Stack Web Developer',
+			company: 'Taylor &amp; Francis',
+			dates: {
+				start: '2016-10-01',
+				end: '2017-17-01'
+			},
+			url: 'https://taylorandfrancis.com/',
+			location: 'Abingdon, UK',
+			summaries: [
+				'I created custom performant WordPress and Expression Engine sites.',
+				'I hand-crafted a node tool to benchmark and monitor Business Sites using the Web Page Test API and display with React.',
+				'Perform performance audits and carry out improvements on business and society sites.'
+			]
+		},
+		photoCrowd: {
+			title: 'Front-end React Developer',
+			company: 'Photocrowd',
+			dates: {
+				start: '2016-03-01',
+				end: '2016-08-01'
+			},
+			url: 'https://photocrowd.com/',
+			location: 'Oxford, UK',
+			summaries: [
+				'I created new components and pages using React/Relay and Less from Photoshop designs.',
+				'I implementing Webpack into the development process to allow for code modularity, linting, and writing future JS (ES2015) code with Babel.',
+				'Applied BEM code methodology for new CSS components.'
+			]
+		},
+		electricStudio: {
+			title: 'Front-end and WordPress Developer',
+			company: 'Electric Studio',
+			dates: {
+				start: '2013-01-01',
+				end: '2016-02-01'
+			},
+			url: 'https://electricstudio.co.uk/',
+			location: 'Abingdon, UK',
+			summaries: [
+				'I created highly customised WordPress sites from Photoshop    designs, with custom post-types, taxonomies, meta boxes, shortcodes and functions.',
+				'The sites were performant, using best practices and technologies, built within budget and in timeframes.',
+				'Pre-launch client training, dealing with any client feedback via Basecamp or telephone.'
+			]
+		},
+		heathWallace: {
+			title: 'Creative UI Developer',
+			company: 'HeathWallace Ltd',
+			dates: {
+				start: '2013-01-01',
+				end: '2016-02-01'
+			},
+			url: '#',
+			location: 'Reading, UK',
+			summaries: [
+				'I built highly accessible websites for clients including HSBC, RBS and Grant Thornton.',
+				'Using CMSs such as WordPress, CQ5 and Sitecore to allow the client to control all content on their site.',
+				'Knowledge Sharing through weekly masterclass sessions (I ran three: WordPress; CSS3; and Sass).'
+			]
+		}
+	};
+</script>
+
 <h1>Andrew JD Hudson - Cirriculum Vitae</h1>
 
 <ul>
@@ -15,12 +114,9 @@
 <h2>Experience</h2>
 
 <ol>
-	<li>Senior Front-end Developer - Scalable Software</li>
-	<li>Senior Front-end Developer - Venture Harbour</li>
-	<li>Full-Stack Software Developer - Taylor and Francis</li>
-	<li>Front-end React Developer - Photocrowd Ltd</li>
-	<li>Front-end and WordPress Developer - Electric Studio</li>
-	<li>Front-end Developer - Heath Wallace</li>
+	{#each Object.values(roles) as role}
+		<li>{role.title} - {role.company}</li>
+	{/each}
 </ol>
 
 <h2>Relevant Projects and Websites</h2>
